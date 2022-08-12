bc12bf77bdc6e35d549b81e33e8de97066ae0691
michaelyuhe <2312744987@qq.com>
2022-08-12 14:55:01 +0800

    Streamlined Code

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,22 @@
 import './popup.css';
 
+function sendMessageToContent(message) {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      const messgae = {
+        type: message,
+      };
+      chrome.tabs.sendMessage(tabs[0].id, messgae, (response) => {
+        console.log(response);
+      });
+    }
+  );
+}
+
 (function () {
   function setUp() {
     chrome.storage.local.set({ mode: 1 });
@@ -7,97 +24,31 @@ import './popup.css';
 
     const setupButton = document.getElementById('setup');
     setupButton.addEventListener('click', () => {
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        (tabs) => {
-          const messgae = {
-            type: 'SETUP',
-          };
-          chrome.tabs.sendMessage(tabs[0].id, messgae, (response) => {
-            console.log(response);
-          });
-        }
-      );
+      sendMessageToContent('SETUP');
     });
 
     const saveButton = document.getElementById('save');
     saveButton.addEventListener('click', () => {
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        (tabs) => {
-          const messgae = {
-            type: 'SAVE',
-          };
-          chrome.tabs.sendMessage(tabs[0].id, messgae, (response) => {
-            console.log(response);
-          });
-        }
-      );
+      sendMessageToContent('SAVE');
     });
 
     const startButton = document.getElementById('start');
     startButton.addEventListener('click', () => {
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        (tabs) => {
-          const message = {
-            type: 'START',
-          };
-          chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
-            console.log(response);
-          });
-        }
-      );
+      sendMessageToContent('START');
       chrome.storage.local.set({ state: 'START' });
     });
 
     const stopButton = document.getElementById('stop');
     stopButton.addEventListener('click', () => {
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        (tabs) => {
-          const message = {
-            type: 'STOP',
-          };
-          chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
-            console.log(response);
-          });
-        }
-      );
+      sendMessageToContent('STOP');
       chrome.storage.local.set({ state: 'STOP' });
     });
 
     const switchButton = document.getElementById('switch');
     switchButton.addEventListener('click', async () => {
       currMode = -currMode;
-
       await chrome.storage.local.set({ mode: currMode });
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        (tabs) => {
-          const message = {
-            type: 'SWITCH',
-          };
-          chrome.tabs.sendMessage(tabs[0].id, message, (response) => {
-            console.log(response);
-          });
-        }
-      );
+      sendMessageToContent('SWITCH');
       switchButton.innerText = String(currMode);
     });
   }
