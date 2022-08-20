cfdcf6951b197a0c2c86c10c93f8497a41066e02
michaelyuhe <2312744987@qq.com>
2022-08-20 13:10:55 +0800

    save params

--- public/options.js
+++ public/options.js
@@ -1,33 +1,39 @@
 (async function () {
-  const params = await chrome.storage.local.get('params');
-  console.log(params);
-
   const scrollStepInput = document.getElementById('scroll-step');
+  const verticalQue = document.getElementById('vertical-que');
+  const horizontalQue = document.getElementById('horizontal-que');
+  const verticalArea = document.getElementById('vertical-area');
+  const horizontalArea = document.getElementById('horizontal-area');
+  const saveButton = document.getElementById('save');
+
+  const params = (await chrome.storage.local.get('params')).params;
+
+  scrollStepInput.value = params.scrollStep || 120;
+  verticalQue.value = params.verticalQue || 5;
+  horizontalQue.value = params.horizontalQue || 30;
+  verticalArea.value = params.verticalArea || 5;
+  horizontalArea.value = params.horizontalArea || 5;
+
   scrollStepInput.addEventListener('input', (e) => {
     params.scrollStep = e.target.value;
   });
 
-  const verticalQue = document.getElementById('vertical-que');
   verticalQue.addEventListener('input', (e) => {
     params.verticalQue = e.target.value;
   });
 
-  const horizontalQue = document.getElementById('horizontal-que');
   horizontalQue.addEventListener('input', (e) => {
     params.horizontalQue = e.target.value;
   });
 
-  const verticalInput = document.getElementById('vertical-area');
-  verticalInput.addEventListener('input', (e) => {
+  verticalArea.addEventListener('input', (e) => {
     params.verticalArea = e.target.value;
   });
 
-  const horizontalInput = document.getElementById('horizontal-area');
-  horizontalInput.addEventListener('input', (e) => {
+  horizontalArea.addEventListener('input', (e) => {
     params.horizontalArea = e.target.value;
   });
 
-  const saveButton = document.getElementById('save');
   saveButton.addEventListener('click', () => {
     console.log(params);
     chrome.storage.local.set({ params: params });
